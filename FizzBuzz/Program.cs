6c31d7b807ace006d052d9ef134b06c8e840a586
Jada <jada.fagan@gmail.com>
2020-06-18 13:31:57 -0400

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for(var idx = 1; idx <= 30; idx++)
+            {
+                if (idx % 3 == 0 && idx % 5 == 0)
+                    Console.Write("FizzBuzz ");
+                else
+                   if (idx % 3 == 0)
+                    Console.Write("Fizz ");
+                else
+                   if (idx % 5 == 0)
+                    Console.Write("Buzz ");
+                else
+                    Console.Write($"{idx} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //{
+            //    for (var nbr = 0; nbr <= 30; nbr++)
+            //        if (nbr % 3 == 0) ;
+
+            //            var nbr =(nbr == true) ? "Fizz" : "";
+            //}
+
+
+        }
+    }
+}
